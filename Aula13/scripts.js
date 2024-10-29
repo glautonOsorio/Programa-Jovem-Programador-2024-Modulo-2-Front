cd158b3a370e9ce699cb3f526073d0eb7fda1c12
GLAUTON OSORIO <glauton.osorio@alunos.sc.senac.br>
2024-10-29 16:20:08 -0300

    feat:aula javascript

---
+++ Aula13/scripts.js
@@ -0,0 +1,22 @@
+/*let question = window.confirm("Continue?");
+
+if (!question) {
+  window.location.href = "https://www.google.com/";
+}
+
+let nome = window.prompt("Digite seu nome:");
+let profissao = window.prompt("Digite sua profissão:");
+let sexo = window.prompt("Digite seu sexo:");
+
+window.alert(`Hello ${nome} então sua profissão é ${profissao} e seu sexo é ${sexo}`);
+
+
+let num1 = window.prompt("Digite o primeiro número: ")
+let num2 = window.prompt("Digite o segundo número: ")
+console.log(parseInt(num1)+ parseInt(num2));
+*/
+
+let nome2 = window.prompt("What is you name cunt?")
+document.write(`Olá ${nome2}. Seu nome possui ${nome2.length} caracteres`)
+document.write(`Olá ${nome2.toUpperCase()}. Seu nome possui ${nome2.length} caracteres`)
+document.write(`Olá ${nome2.toLowerCase()}. Seu nome possui ${nome2.length} caracteres`)
